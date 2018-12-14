4d0a04e0b8a8d3eb030bbbb9a4144801d80db6bd
user1 <user1@A518-13>
2018-12-14 12:01:40 +0300

    ok

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,7 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Первое знакомство GitHub");
-            Console.WriteLine("Первое знаком");
+            Console.WriteLine("Семен - прошлое");
         }
     }
 }
